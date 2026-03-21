09914386b16d406370fb011946e7e111066533cc
Mania-Ming <amaniamaepearl99@gmail.com>
2026-03-21 13:27:17 +0800

    fixing error

--- lib/api/axiosInstance.ts
+++ lib/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api"
-
+// const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api"
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL
 const api = axios.create({
   baseURL: BASE_URL,
   headers: { "Content-Type": "application/json" },
